4760be065b7560090202343ac876820eedf91f2a
weijunext <weijunext@gmail.com>
2025-09-17 17:37:09 +0800

    feat: Disable Next.js img element linting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
     "react-hooks/exhaustive-deps":
       process.env.NODE_ENV === "production" ? "off" : "warn",
     "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off",
   },
 };
