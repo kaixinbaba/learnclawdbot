d88351b48e66366244623a4d308234e85dc98419
weijunext <weijunext@gmail.com>
2025-12-28 22:36:48 +0800

    fix: change header element to div in Newsletter component

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -61,7 +61,7 @@ export function Newsletter() {
   };
   return (
     <div className="">
-      <h3 className="mb-3 font-semibold">{t("title")}</h3>
+      <div className="mb-3 font-semibold">{t("title")}</div>
       <p className="text-sm mb-3">{t("description")}</p>
       <form onSubmit={handleSubscribe} className="flex flex-col gap-2 max-w-64">
         <div className="flex">
